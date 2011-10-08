26b3757b8733f2326db8a7881a2a8fc65bcc75c5
Albert Pascual <alpascual@gmail.com>
2011-10-08 11:26:39 -0700

    Initial

---
+++ learn2read/LearnRead/iPad/LearnReadAppDelegate_iPad.h
@@ -0,0 +1,13 @@
+//
+//  LearnReadAppDelegate_iPad.h
+//  LearnRead
+//
+//  Created by Albert Pascual on 10/7/11.
+//  Copyright 2011 Al. All rights reserved.
+//
+
+#import "LearnReadAppDelegate.h"
+
+@interface LearnReadAppDelegate_iPad : LearnReadAppDelegate
+
+@end
